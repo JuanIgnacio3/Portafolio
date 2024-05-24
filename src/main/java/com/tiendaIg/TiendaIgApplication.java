1e41ef23f8c0929b4794c3c5a0b3eac6c56cff99
Ignacio <Ignacio@DESKTOP-108IMK1>
2024-05-23 23:18:51 -0600

    Cración proyecto inicial semana 01

---
+++ src/main/java/com/tiendaIg/TiendaIgApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaIg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaIg/TiendaIgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaIg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
